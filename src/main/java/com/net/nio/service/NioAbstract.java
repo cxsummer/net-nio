d4f756925955fb54536624695517dce6035cd812
caojiancheng <jiancheng.cao@yi23.net>
2021-05-03 10:27:52 +0800

    UP:抽象nio

--- src/main/java/com/net/nio/service/NioAbstract.java
+++ src/main/java/com/net/nio/service/NioAbstract.java
@@ -26,19 +26,20 @@ public abstract class NioAbstract {
     public NioAbstract() {
         try {
             selector = Selector.open();
-            threadLocal.submit(this::nioMonitor);
+            threadLocal.submit(this::startNioMonitor);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
     /**
-     * 如果执行selector.select的方法的线程和向selector注册socketChannel的线程不是同一个线程的话
-     * 那么selector.select就不能阻塞，因为一旦selector阻塞，而根据对象的可重入性，那么别的线程需要等待锁的释放
-     * 但是如果selector.select查询不到可用的连接，那么就不会释放锁，而只有向selector注册连接才会有可用连接，这样就会造成死锁
-     * 所以要用selector.selectNow方法，不阻塞
+     * 因为select方法执行的是lockAndDoSelect方法，里面是用的synchronized锁住的SelectorImpl类的publicKeys变量
+     * 那么selector.select就不能阻塞，因为一旦selector阻塞，而根据锁的可重入性，当前线程可以直接执行但是别的线程需要等待锁的释放
+     * select先执行，如果selector.select查询不到可用的连接，那么就不会释放锁，而只有向selector注册连接才会有可用连接
+     * 即socketChannel.register(selector)，这个方法也需要获取publicKeys对象锁，又因为是其他线程，这样就会造成死锁
+     * 所以要用selector.selectNow方法，查询到或者查不到都会释放锁，不会在获取到锁后阻塞
      */
-    private void nioMonitor() {
+    private void startNioMonitor() {
         while (!Thread.currentThread().isInterrupted()) {
             try {
                 if (selector.selectNow() <= 0) {
